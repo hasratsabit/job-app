d240f01c4452dcb59c7eea4323fa9e1769fe533d
Hasrat <hsabit111@gmail.com>
2018-07-30 00:38:40 -0400

    The register form validation is not done yet

--- my-app/src/app/core/register/employer-register/employer-register.component.ts
+++ my-app/src/app/core/register/employer-register/employer-register.component.ts
@@ -1,11 +1,32 @@
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+
+
+
+
+function valueMatcher(firstValue, secondValue) {
+  return (control: AbstractControl): { [key: string]: boolean} | null => {
+    let emailControl = control.get(firstValue);
+    let confirmEmailControl = control.get(secondValue);
+  
+    if(emailControl.pristine || confirmEmailControl.pristine) {
+      return null;
+    }
+  
+    if(emailControl.value === confirmEmailControl.value) {
+      return null;
+    }
+    return { 'match': true }
+  }
+}
+
 
 @Component({
   selector: 'app-employer-register',
   templateUrl: './employer-register.component.html',
   styleUrls: ['./employer-register.component.scss']
 })
+
 export class EmployerRegisterComponent implements OnInit {
 
   public employerForm: FormGroup;
@@ -18,18 +39,36 @@ export class EmployerRegisterComponent implements OnInit {
    }
 
 
-  createEmployerRegisteration() {
+  createEmployerRegisteration(): void {
     this.employerForm = this.formBuilder.group({
       name: ['', Validators.required],
       username: ['', Validators.required],
-      email: ['', Validators.required],
-      confirmEmail: ['', Validators.required],
       companyName: ['', Validators.required],
       companySize: ['', Validators.required],
-      serviceCategory: ['', Validators.required],
+      // serviceCategory: ['', Validators.required],
+      emailGroup: this.formBuilder.group({
+        email: ['', Validators.required],
+        confirmEmail: ['', Validators.required],
+      }, {validator: valueMatcher('email', 'confirmEmail')}),
       password: ['', Validators.required],
       confirmPassword: ['', Validators.required],
     })
+  }
+
+  onRegisterEmployer() {
+    const employer = {
+      name: this.employerForm.get('name'),
+      username: this.employerForm.get('username'),
+      companyName: this.employerForm.get('companyName'),
+      companySize: this.employerForm.get('companySize'),
+      // serviceCategory: this.employerForm.get('serviceCategory'),
+      email: this.employerForm.get('email'),
+      confirmEmail: this.employerForm.get('confirmEmail'),
+      password: this.employerForm.get('password'),
+      confirmPassword: this.employerForm.get('confirmPassword'),
+    }
+
+    console.log(employer);
   }
 
   ngOnInit() {
