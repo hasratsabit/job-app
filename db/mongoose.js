de875ddf4afbab8e26bbcbc38d4a724486086864
Hasrat <hsabit111@gmail.com>
2018-08-04 10:59:43 -0400

    The login route is fixed

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27018/job-app', (err) => {
+mongoose.connect('mongodb://hasratsabit:Lima1234@ds018248.mlab.com:18248/job-app', { useNewUrlParser: true}, (err) => {
   if(err) {
     return console.log(`Error occurred starting the database ${err}`);
   }
--- router/routes/employers.js
+++ router/routes/employers.js
@@ -2,26 +2,39 @@
 const express = require('express');
 const router = express.Router();
 const Employer = require('../../models/employer');
+const _ = require('lodash');
 
 
-router.post('/', async (req, res) => {
+router.post('/', (req, res) => {
+  const employer = new Employer({
+    name: req.body.name,
+    username: req.body.username,
+    companyName: req.body.companyName,
+    companySize: req.body.companySize,
+    email: req.body.emailGroup.email,
+    password: req.body.password
+  });
+
+  employer.save().then(() => {
+    return employer.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).json({ success: true, message: `Your account has been successfully created.`})
+  }).catch((err) => {
+    res.json({ success: false, message: `Error occurred: ${err}`});
+  })
+});
+
+
+router.post('/login', async (req, res) => {
   try {
-    const employer = new Employer({
-      name: req.body.name,
-      username: req.body.username,
-      companyName: req.body.companyName,
-      companySize: req.body.companySize,
-      email: req.body.emailGroup.email,
-      password: req.body.password
-    })
-
-    await employer.save();
-    res.send({ success: true, message: `Congratulation ${req.body.name}. Your account has been successfully created.`});
-  } catch (err) {
-    res.status(400).send({ success: false, message: `Error occured: ${err}`});
+    const body = _.pick(req.body, ['username', 'password']);
+    const user = await Employer.findByCredentials(body.username, body.password);
+    const token = await user.generateAuthToken();
+    res.header('x-auth', token).json({ success: true, message: 'Successfully logged in.'});
+  } catch(err) {
+    res.json({ success: false, message: `Error occurred: ${err}`});
   }
 })
-
 
 
 module.exports = router;
